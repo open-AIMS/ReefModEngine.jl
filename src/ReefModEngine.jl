a3e2c454bc440e3c5076633f06740050a000925b
Takuya Iwanaga <takuyai@gmail.com>
2023-03-23 17:53:10 +1100

    Fix deployment area calculation and add typing

--- src/ReefModEngine.jl
+++ src/ReefModEngine.jl
@@ -52,44 +52,54 @@ function init_rme(rme_path::String)
     @info "Loaded $rme_vers"
 end
 
-function deployment_area(n_corals::Int64, density::Float64, target_areas::Vector{Float64})::Float64
-    m2_TO_km2 = 0.000001
+function reset_rme()
+    global RME_BASE_GRID_SIZE = 100
 
+    @RME ivRemoveAll()::Cvoid
+    @RME reefSetRemoveAll()::Cint
+end
+
+function area_needed(n_corals::Int64, density::Float64)::Float64
     # ReefMod deploys twice a year so halve the number of corals to deploy
-    d_area = ((n_corals * 0.5) / density) * m2_TO_km2  # Convert m² to km²
-    d_area_pct = (d_area / sum(target_areas)) * 100.0
+    return ((n_corals * 0.5) / density) * m2_TO_km2  # Convert m² to km²
+end
+
+function deployment_area(n_corals::Int64, max_n_corals::Int64, density::Float64, target_areas::Vector{Float64})::Tuple{Float64,Float64}
+    req_area = area_needed(max_n_corals, density)
+    mod_density = (n_corals * 0.5) / (req_area / m2_TO_km2)
+    d_area_pct = (req_area / sum(target_areas)) * 100.0
 
     # Using a global var here to stop inappropriate small grid size from
     # being selected once a larger grid size is set.
     # TODO: Should be an input.
     global RME_BASE_GRID_SIZE
-    MIN_CELLS = 3
-    if (RME_BASE_GRID_SIZE * d_area / sum(target_areas)) < MIN_CELLS
+    min_cells::Int64 = 3
+    if (RME_BASE_GRID_SIZE * req_area / sum(target_areas)) < min_cells
         # Determine new grid resolution in terms of number of N by N cells
-        target_grid_size = 3 * sum(target_areas) / d_area
-        cell_res = ceil(sqrt(target_grid_size))  # new cell resolution
+        target_grid_size::Float64 = 3.0 * sum(target_areas) / req_area
+        cell_res::Int64 = ceil(Int64, sqrt(target_grid_size))  # new cell resolution
 
         # RME supported cell sizes (N by N)
-        p = Int64[10, 20, 25, 30, 36, 43, 55, 64, 85, 100]
-        n_cells = first(p[p.>=cell_res])
+        p::Vector{Int64} = Int64[10, 20, 25, 30, 36, 43, 55, 64, 85, 100]
+        n_cells::Int64 = first(p[p.>=cell_res])
         RME_BASE_GRID_SIZE = n_cells * n_cells
-        opt = "RMFAST$(first(p[p .>= cell_res]))"
+        opt::String = "RMFAST$(first(p[p .>= cell_res]))"
         @RME setOptionText("emulation_method"::Cstring, opt::Cstring)::Cint
 
-        @warn "Insufficient number of treatment cells. Adjusting grid size.\nSet grid to $(n_cells) by $(n_cells) cells"
+        @warn "Insufficient number of treatment cells. Adjusting grid size.\nSetting grid to $(n_cells) by $(n_cells) cells"
     end
 
-    return d_area_pct
+    return d_area_pct, mod_density
 end
 
-function set_seeding_deployment(name::String, reefset::String, n_corals::Int64, first_year::Int64, last_year::Int64, year_step::Int64, area_km2::Vector{Float64}, density::Float64)
+function set_seeding_deployment(name::String, reefset::String, n_corals::Int64, max_effort::Int64, first_year::Int64, last_year::Int64, year_step::Int64, area_km2::Vector{Float64}, density::Float64)
     iv_type = "outplant"
 
-    area_pct = deployment_area(n_corals, density, area_km2)
+    area_pct, mod_density = deployment_area(n_corals, max_effort, density, area_km2)
 
     @RME ivAdd(name::Cstring, iv_type::Cstring, reefset::Cstring, first_year::Cint, last_year::Cint, year_step::Cint)::Cint
     @RME ivSetOutplantAreaPct(name::Cstring, area_pct::Cdouble)::Cint
-    @RME ivSetOutplantCountPerM2(name::Cstring, density::Cdouble)::Cint
+    @RME ivSetOutplantCountPerM2(name::Cstring, mod_density::Cdouble)::Cint
 end
 
 export init_rme, @RME
