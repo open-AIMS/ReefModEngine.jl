67554e4f9420c8f965cb128bebc651492e5942bc
Takuya Iwanaga <takuyai@gmail.com>
2024-04-04 18:59:13 +1100

    Initial docs setup

---
+++ docs/make.jl
@@ -0,0 +1,34 @@
+using Documenter, DocumenterVitepress
+using ReefModEngine
+using Logging
+
+Logging.disable_logging(Logging.Warn)
+
+makedocs(;
+    sitename="ReefModEngine.jl",
+    modules=[ReefModEngine],
+    clean=true,
+    doctest=true,
+    authors="Iwanaga et al.",
+    checkdocs=:all,
+    format=DocumenterVitepress.MarkdownVitepress(
+        repo = "github.com/open-AIMS/ReefModEngine.jl", # this must be the full URL!
+        devbranch="main",
+        devurl="dev";
+    ),
+    draft=false,
+    source="src",
+    build="build",
+    warnonly=true,
+)
+
+# Enable logging to console again
+Logging.disable_logging(Logging.BelowMinLevel)
+
+deploydocs(;
+    repo="github.com/open-AIMS/ReefModEngine.jl.git",
+    target="build", # this is where Vitepress stores its output
+    branch = "gh-pages",
+    devbranch="main",
+    push_preview = true
+)
