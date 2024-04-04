e413c7fe8147008bb412447a1f73c6241d2fb074
Takuya Iwanaga <takuyai@gmail.com>
2024-04-04 19:28:56 +1100

    Add support for matching IDs to RME's reef list position index

--- src/interface.jl
+++ src/interface.jl
@@ -23,3 +23,44 @@ function reef_ids()::Vector{String}
 
     return reef_id_list
 end
+
+"""
+    match_id(id::String)::Int64
+    match_ids(ids::Vector{String})::Vector{Int64}
+
+Find matching index position for the given ID(s) according to ReefMod Engine's reef list.
+
+# Note
+
+ReefMod Engine's reef list is in all upper case. The provided IDs are converted to
+upper case to ensure a match.
+
+# Examples
+
+```julia
+julia> reef_ids()
+# 3806-element Vector{String}:
+#  "10-330"
+#  "10-331"
+#  ⋮
+#  "23-048"
+#  "23-049"
+
+julia> match_id("10-330")
+#  1
+
+julia> match_id("23-049")
+#  3806
+
+julia> match_ids(["23-048", "10-331"])
+#  3805
+#  2
+```
+"""
+function match_id(id::String)::Int64
+    rme_ids = reef_ids()
+    return findfirst(==(uppercase(id)), rme_ids)
+end
+function match_ids(ids::Vector{String})::Vector{Int64}
+    return match_id.(ids)
+end
