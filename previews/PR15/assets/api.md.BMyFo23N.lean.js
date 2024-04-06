21eee452e7bfd4999af9dc2a375f1f82e6eca0bb
Documenter.jl <documenter@juliadocs.github.io>
2024-04-06 02:56:56 +0000

    build based on bb7d791

--- previews/PR15/assets/api.md.BMyFo23N.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as i,c as s,o as e,a7 as a}from"./chunks/framework.Dxv0vFf2.js";const E=JSON.parse('{"title":"API","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),n={name:"api.md"},t=a("",29),l=[t];function p(h,r,d,k,o,g){return e(),s("div",null,l)}const y=i(n,[["render",p]]);export{E as __pageData,y as default};
