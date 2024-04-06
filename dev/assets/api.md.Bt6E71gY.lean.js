fee185f238af88c39bc83606f5727b0d5c6616aa
Documenter.jl <documenter@juliadocs.github.io>
2024-04-06 01:32:51 +0000

    build based on 6f76be6

---
+++ dev/assets/api.md.Bt6E71gY.lean.js
@@ -0,0 +1,1 @@
+import{_ as i,c as s,o as a,a7 as e}from"./chunks/framework.BwCmwMP0.js";const E=JSON.parse('{"title":"API","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),n={name:"api.md"},t=e("",21),l=[t];function p(h,r,d,k,o,g){return a(),s("div",null,l)}const y=i(n,[["render",p]]);export{E as __pageData,y as default};
