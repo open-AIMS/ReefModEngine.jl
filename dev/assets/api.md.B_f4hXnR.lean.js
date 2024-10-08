afb4bffa253423d6f70dc3cb0dfdf2686a40654b
Documenter.jl <documenter@juliadocs.github.io>
2024-10-08 07:32:24 +0000

    build based on 30e7168

--- dev/assets/api.md.B_f4hXnR.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as i,c as s,o as a,a7 as e}from"./chunks/framework.BFNEIGBL.js";const E=JSON.parse('{"title":"API","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),t={name:"api.md"},n=e("",49),l=[n];function p(h,r,d,k,o,g){return a(),s("div",null,l)}const y=i(t,[["render",p]]);export{E as __pageData,y as default};
