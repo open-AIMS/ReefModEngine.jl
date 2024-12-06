fe2749340042c1040cf034a70e8e05166ee70f0f
Rose Crocker <r.crocker@aims.gov.au>
2024-12-06 15:56:18 +1000

    Fold version checking into function

--- src/deployment.jl
+++ src/deployment.jl
@@ -1,6 +1,12 @@
-rme_ver = @RME version()::Cstring
-version_vec = parse.(Int64, split(rme_ver, '.'))
-const ver_check = (version_vec[3] <= 28) && (version_vec[1] == 1)
+"""
+    version_check()::Vector{Int64}
+
+Check RME version
+"""
+function version_check()::Vector{Int64}
+    rme_ver = @RME version()::Cstring
+    return parse.(Int64, split(rme_ver, '.'))
+end
 
 """
     deployment_area(n_corals::Int64, max_n_corals::Int64, density::Union{Float64, Vector{Float64}}, target_areas::Vector{Float64})::Tuple{Float64,Float64}
@@ -94,7 +100,8 @@ function deployment_area(
     deployment_area_pct = min((req_area / sum(target_areas)) * 100.0, 100.0)
 
     # In RME versions higher than 1.0.28 density needs to be a vector with each element representing density per species
-    density = ver_check ? density : fill(density, 6)
+    version_vec = version_check()
+    density = (version_vec[3] <= 28) && (version_vec[1] == 1) ? density : fill(density, 6)
     return deployment_area_pct, density
 end
 
@@ -165,7 +172,8 @@ function set_outplant_deployment!(
     )::Cint
     @RME ivSetOutplantAreaPct(name::Cstring, area_pct::Cdouble)::Cint
 
-    if !ver_check
+    version_vec = version_check()
+    if !((version_vec[3] <= 28) && (version_vec[1] == 1))
         @RME ivSetOutplantCountPerM2(
             name::Cstring, mod_density::Vector{Cdouble}, length(mod_density)::Cint
         )::Cint
@@ -228,7 +236,8 @@ function set_outplant_deployment!(
     )::Cint
     @RME ivSetOutplantAreaPct(name::Cstring, area_pct::Cdouble)::Cint
 
-    if !ver_check
+    version_vec = version_check()
+    if !((version_vec[3] <= 28) && (version_vec[1] == 1))
         @RME ivSetOutplantCountPerM2(
             name::Cstring, mod_density::Vector{Cdouble}, length(mod_density)::Cint
         )::Cint
