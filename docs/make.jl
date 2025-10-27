12785013f740a0d63a060b92d3309226d3d7d8a8
DanTanAtAims <d.tan@aims.gov.au>
2025-10-27 16:59:23 +1100

    fix: deployment vitepress is not compatible with Documenter anymore

--- docs/make.jl
+++ docs/make.jl
@@ -20,16 +20,23 @@ makedocs(;
     draft=false,
     source="src",
     build="build",
-    warnonly=true
+    pages=[
+         "index.md",
+         "Getting Started"=>"getting_started.md",
+         "Options"=>"options.md",
+         "Parallelisation"=>"parallelisation.md",
+         "Results"=>"results_store.md",
+    ]
 )
 
 # Enable logging to console again
 Logging.disable_logging(Logging.BelowMinLevel)
 
-deploydocs(;
-    repo="github.com/open-AIMS/ReefModEngine.jl.git",
-    target="build", # this is where Vitepress stores its output
+# VitePress not compatible with Documenter anymore
+DocumenterVitepress.deploydocs(;
+    repo = "github.com/open-AIMS/ReefModEngine.jl",
+    target = joinpath(@__DIR__, "build"),
     branch = "gh-pages",
-    devbranch="main",
-    push_preview=false
+    devbranch = "main",
+    push_preview = true,
 )
